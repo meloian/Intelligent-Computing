6de378365c79033920d26bd7577e6b31a5cafbfd
meloian <myrosmelo-ipt27@lll.kpi.ua>
2025-06-09 05:09:29 +0300

    cgw: remove unnecessary stuff

--- cgw/clean_widgets.py
+++
@@ -1,9 +0,0 @@
-import nbformat
-
-nb = nbformat.read("ic_cgw.ipynb", as_version=4)
-# Прибираємо глобальний metadata.widgets
-nb.metadata.pop("widgets", None)
-# Прибираємо metadata.widgets у кожній клітинці
-for cell in nb.cells:
-    cell.metadata.pop("widgets", None)
-nbformat.write(nb, "ic_cgw.ipynb")
